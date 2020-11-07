e56bdf0fc24ab12186418f96f579084c854b8dcf
Christopher Hernandez <cerberean661@gmail.com>
2020-11-06 18:29:25 -0800

    ex2 complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,30 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
+    hash_table = {}
+
+    for i in tickets:
+        hash_table[i.source] = i.destination
+
+    route = [hash_table["NONE"]]
+
+    for i in range(1, length):
+        route.append(hash_table[route[i - 1]])
+
     return route
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets_list = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+print(reconstruct_trip(tickets_list, 10))
