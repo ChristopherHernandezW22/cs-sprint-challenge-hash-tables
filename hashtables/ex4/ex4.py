802169c6b0dc52591d3f4bd7cb718113c9a2694e
Christopher Hernandez <cerberean661@gmail.com>
2020-11-13 19:02:11 -0800

    ex4 redone, test should now pass

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,35 +1,48 @@
-def has_negatives(arr, a):
+def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
-    values = []
 
-    # for each item in array
-    for i in range(a):
+    # Creating hash table
+    # key: number, value: boolean (list containing negation)
+    ht = {}
 
-        # finding the negative value of array[i] from i + 1 to
-        for j in range(i + 1, a):
+    # assign result variable to empty array
+    result = []
 
-            # if the absolute value matches, append
-            if(abs(arr[i])) == abs(arr[j]):
-                values.append(abs(arr[i]))
+    # Loop through numbers:
+    for num in a:
+        # If number is greater than 0, it's a positive number
+        if num > 0:
+            # If a positive number is not in hash table
+            if num not in ht:
+                # Make it False
+                ht[num] = False
+            # If the negative version of that number is in ht
+            if -num in ht:
+                # Make it True
+                ht[-num] = True
+                ht[num] = True
+        # Else it's a negative number
+        else:
+            # If a negative number is not in hash table
+            if num not in ht:
+                # Make it False
+                ht[num] = False
+            # If the absolute number version of that negative number is in ht
+            if abs(num) in ht:
+                # Make it True
+                ht[abs(num)] = True
+                ht[num] = True
 
-    # print "0" if size of vector is 0
-    if (len(values) == 0):
-        return;
+    # Iterate through the entries
+    for num, boolean in ht.items():
+        if boolean is True and num > 0:
+            result.append(num)
 
-    values.sort()
-
-    # printing negative and positive pairings
-    for i in range(len(values)):
-        # print(-values[i], "", values[i], end = " ") # print pairs
-        print(values[i], end = " ") # print positives that contains negatives
-
-    # return result
+    return result
 
 if __name__ == "__main__":
-    # print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
-    arr = [-1, -2, 1, 2, 3, 4, -4]
-    a = len(arr)
-    has_negatives(arr, a)
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    
